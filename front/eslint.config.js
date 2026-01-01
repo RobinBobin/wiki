485f11d7a97d5977548552906bfbc5d54c1a59f2
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-01 13:35:08 +0300

    `@robinbobin/prettier-config`

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,6 +1,6 @@
-import parentConfig from "@robinbobin/eslint-config-react-native";
-import { defineConfig } from "eslint/config";
+import parentConfig from '@robinbobin/eslint-config-react-native'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig(parentConfig, {
-  ignores: ["dist/*"],
-});
+  ignores: ['dist/*']
+})
