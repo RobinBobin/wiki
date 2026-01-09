79344a85efa2ae2f087f393fb96c603e16cf65a5
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-09 21:32:10 +0300

    Front: `src/helpers/{handleError/index.ts -> handleError.ts}`.

---
+++ front/src/helpers/handleError.ts
@@ -0,0 +1,12 @@
+import { isError } from 'radashi'
+
+import { alert } from './alert'
+
+export const handleError = (error: unknown): void => {
+  const message =
+    isError(error) ?
+      error.message
+    : `An unknown error of type ${typeof error}: ${String(error)}`
+
+  alert(message)
+}
--- front/src/helpers/handleError/index.ts
+++
@@ -1,5 +0,0 @@
-import { alert } from '../alert'
-
-export const handleError = (error: unknown): void => {
-  alert((error as Error).message)
-}
