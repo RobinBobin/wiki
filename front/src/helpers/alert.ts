b7174642f13d6cf99a40eed15991fd3579a8b2b0
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-09 21:29:31 +0300

    Front: `@blazejkustra/react-native-alert@^1.0.0` added.

--- front/src/helpers/alert.ts
+++ front/src/helpers/alert.ts
@@ -1,4 +1,4 @@
-import { Alert } from 'react-native'
+import Alert from '@blazejkustra/react-native-alert'
 
 export const alert = (message: string, title = ''): void => {
   Alert.alert(title, message, [{ text: 'OK' }], { cancelable: true })
