1b355013f95ce6d8a963e1547b35c2b086cf9c59
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-10 23:06:03 +0300

    Back: `gin/ws/route.go`: `route()` simplified.

--- back/gin/ws/route.go
+++ back/gin/ws/route.go
@@ -3,36 +3,26 @@ package ws
 import (
 	"log"
 
-	articlesv1 "wiki/gen/wiki/articles/v1"
 	envelopev1 "wiki/gen/wiki/envelope/v1"
 
 	"wiki/gin/ws/article"
 )
 
-type responseType interface {
-	*articlesv1.CreateArticleResponse |
-		*articlesv1.SearchArticlesResponse
-}
-
-func wrap[TResponseType responseType](rawResponse TResponseType) *envelopev1.ServerEnvelope {
-	switch response := any(rawResponse).(type) {
-	case *articlesv1.CreateArticleResponse:
-		return &envelopev1.ServerEnvelope{Payload: &envelopev1.ServerEnvelope_CreateArticle{CreateArticle: response}}
-
-	case *articlesv1.SearchArticlesResponse:
-		return &envelopev1.ServerEnvelope{Payload: &envelopev1.ServerEnvelope_SearchArticles{SearchArticles: response}}
-	}
-
-	return nil
-}
-
 func route(envelope *envelopev1.ClientEnvelope) *envelopev1.ServerEnvelope {
 	switch payload := envelope.GetPayload().(type) {
 	case *envelopev1.ClientEnvelope_CreateArticle:
-		return wrap(article.Create(payload.CreateArticle))
+		return &envelopev1.ServerEnvelope{
+			Payload: &envelopev1.ServerEnvelope_CreateArticle{
+				CreateArticle: article.Create(payload.CreateArticle),
+			},
+		}
 
 	case *envelopev1.ClientEnvelope_SearchArticles:
-		return wrap(article.Search(payload.SearchArticles))
+		return &envelopev1.ServerEnvelope{
+			Payload: &envelopev1.ServerEnvelope_SearchArticles{
+				SearchArticles: article.Search(payload.SearchArticles),
+			},
+		}
 
 	default:
 		log.Printf("Unknown payload type: %T", payload)
