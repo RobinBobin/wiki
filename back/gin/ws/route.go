5478fd0ff10454a58631e0ce064feaccc3b74914
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-13 00:21:20 +0300

    Back: `gin/ws/route.go`: `envelopev1.ServerEnvelope_DefaultResponse` used.

--- back/gin/ws/route.go
+++ back/gin/ws/route.go
@@ -1,8 +1,8 @@
 package ws
 
 import (
-	"log"
-
+	"wiki/gen/google/rpc"
+	defresponsev1 "wiki/gen/wiki/defresponse/v1"
 	envelopev1 "wiki/gen/wiki/envelope/v1"
 
 	"wiki/gin/ws/article"
@@ -25,8 +25,16 @@ func route(envelope *envelopev1.ClientEnvelope) *envelopev1.ServerEnvelope {
 		}
 
 	default:
-		log.Printf("Unknown payload type: %T", payload)
+		return &envelopev1.ServerEnvelope{
+			Payload: &envelopev1.ServerEnvelope_DefaultResponse{
+				DefaultResponse: &defresponsev1.DefaultResponse{
+					Status: &rpc.Status{
+						Code:    int32(rpc.Code_UNIMPLEMENTED),
+						Message: "Unknown payload type",
+					},
+				},
+			},
+		}
 
-		return nil
 	}
 }
