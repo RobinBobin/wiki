0639d92b5e141dd6ca9500570f2c05b6b0b8f846
Ruben Shalimov <r_shalimov@inbox.ru>
2026-01-13 20:21:02 +0300

    Back: `db.SearchArticles()`: `ORDER BY` added.

--- back/db/search_articles.go
+++ back/db/search_articles.go
@@ -7,7 +7,12 @@ import (
 func SearchArticles(query string) []article {
 	var articles []article
 
-	db.Select("id, title, body").Where("title like ?", fmt.Sprintf("%v%%", query)).Limit(10).Find(&articles)
+	db.
+		Select("id, title, body").
+		Where("title like ?", fmt.Sprintf("%v%%", query)).
+		Limit(10).
+		Order("title").
+		Find(&articles)
 
 	return articles
 }
